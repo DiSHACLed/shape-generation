4a76abdef562a6e6d067ef6cb85ba8ed16a6b2ef
Koen Jacobs <koen.jacobs@redpencil.io>
2026-04-22 12:05:17 +0200

    clean + update ext folder

--- external/download.sh
+++ external/download.sh
@@ -5,7 +5,12 @@ if [ "$(basename "$(pwd)")" != "external" ]; then
     exit 1
 fi
 
+# java bin
+wget "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.18%2B8/OpenJDK17U-jre_x64_linux_hotspot_17.0.18_8.tar.gz"
+tar -xvzf OpenJDK17U-jre_x64_linux_hotspot_17.0.18_8.tar.gz
+
 # shacl play
-wget "https://github.com/sparna-git/shacl-play/releases/download/0.11.7/shacl-play-app-0.11.7-onejar.jar"
+wget "https://github.com/sparna-git/shacl-play/releases/download/0.12.0/shacl-play-app-0.12.0-onejar.jar"
+
 # QSE
 git clone https://github.com/dkw-aau/qse/
